537771a1ed95e9805f45423d8852e3cb4dfb98f5
Youssef hamad <youssefhamad007@gmail.com>
2025-05-12 16:52:33 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
   plugins: [react(),
       tailwindcss(),
   ],
-  base: "/Movies4u",
+  base: "/Movies4u/",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
